f13fe341d2cfd98a717ced1e83d0901db50f76cd
Daniel Orozco <dany.orozco.lomeli@gmail.com>
2021-11-20 15:01:32 -0800

    mega tablas newton terminadas

--- main.c
+++ main.c
@@ -5,14 +5,15 @@
 #include "include/secante.h"
 #include "include/misc.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void playgroundbiseccion(void);
 void playgroundpuntofijo(void);
 void playgroundsecante(void);
 void playgroundnewton(void);
 void metodonewtonejercicio3b(void);
-//void metodonewtonejercicio3b(double totalcalculos[][13], int raices, double beta[], 
-   // double gamma[], double alpha[], double b[],double objetivo, int variante,int maxit);
+
 
 int main(void)
 {
@@ -71,72 +72,91 @@ void metodonewtonejercicio3b(void)
     int variante = EXACTAFUNCIONDIFICIL;
     int maxit = MAXIT64;
     double objetivo = OBJETIVO;
-    int i,nbeta,ngamma,nalpha,nb,raices;
+    int i,j,nbeta,ngamma,nalpha,nb,raices;
 
     double beta[5] = {-3,-2,-2,-1,1},gamma[5] = {-2,-1,-1,0,2};
     double alpha[5] = {-2.2,-1.6,-1.4,-0.76,1.76},b[5] = {-2.25,-1.65,-1.45,-0.81,1.65};
 
-    double totalcalculos[maxit][13];
+    double calculosraiz_i[maxit][13];
     double calculosbeta[maxit][2],calculosgamma[maxit][2],calculosalpha[maxit][2];
     double calculosb[maxit][2];
-    
-    nbeta = metodonewton(calculosbeta, beta[0],objetivo,variante,0,maxit);
-    ngamma = metodonewton(calculosgamma, gamma[0],objetivo,variante,0,maxit);
-    nalpha = metodonewton(calculosalpha, alpha[0],objetivo,variante,0,maxit);
-    nb = metodonewton(calculosb, b[0],objetivo,variante,0,maxit);
 
-    for(i = 0;i < nbeta;i++)
+    char nombretabla[90];
+    char nombretablalatex[90];
+    char buffer[20];
+
+    for(j=0;j<5;j++)
     {
-        totalcalculos[i][0] = i;
-        totalcalculos[i][1] = calculosbeta[i][0];
-        totalcalculos[i][2] = calculosbeta[i][1];
-        totalcalculos[i][3] = fabs(calculosbeta[i+1][0] - calculosbeta[i][0]); //cuidado con el ultimo
-    }
+        nbeta = metodonewton(calculosbeta, beta[j],objetivo,variante,0,maxit);
+        ngamma = metodonewton(calculosgamma, gamma[j],objetivo,variante,0,maxit);
+        nalpha = metodonewton(calculosalpha, alpha[j],objetivo,variante,0,maxit);
+        nb = metodonewton(calculosb, b[j],objetivo,variante,0,maxit);
 
-    if(i < maxit)
-    {
-        rellenarcerostabla(totalcalculos,i,maxit,1,2,3);
-    }
+        for(i = 0;i < nbeta;i++)
+        {
+            calculosraiz_i[i][0] = i;
+            calculosraiz_i[i][1] = calculosbeta[i][0];
+            calculosraiz_i[i][2] = calculosbeta[i][1];
+            calculosraiz_i[i][3] = fabs(calculosbeta[i+1][0] - calculosbeta[i][0]); //cuidado con el ultimo
+        }
 
-    for(i = 0;i < ngamma;i++)
-    {
-        totalcalculos[i][0] = i;
-        totalcalculos[i][4] = calculosgamma[i][0];
-        totalcalculos[i][5] = calculosgamma[i][1];
-        totalcalculos[i][6] = fabs(calculosgamma[i+1][0] - calculosgamma[i][0]); //cuidado con el ultimo
-    }
+        if(i < maxit)
+        {
+            rellenarcerostabla(calculosraiz_i,i,maxit,1,2,3);
+        }
 
-    if(i < maxit)
-    {
-        rellenarcerostabla(totalcalculos,i,maxit,4,5,6);
-    }
+        for(i = 0;i < ngamma;i++)
+        {
+            calculosraiz_i[i][0] = i;
+            calculosraiz_i[i][4] = calculosgamma[i][0];
+            calculosraiz_i[i][5] = calculosgamma[i][1];
+            calculosraiz_i[i][6] = fabs(calculosgamma[i+1][0] - calculosgamma[i][0]); //cuidado con el ultimo
+        }
 
-    for(i = 0;i < nalpha;i++)
-    {
-        totalcalculos[i][0] = i;
-        totalcalculos[i][7] = calculosalpha[i][0];
-        totalcalculos[i][8] = calculosalpha[i][1];
-        totalcalculos[i][9] = fabs(calculosalpha[i+1][0] - calculosalpha[i][0]); //cuidado con el ultimo
-    }
+        if(i < maxit)
+        {
+            rellenarcerostabla(calculosraiz_i,i,maxit,4,5,6);
+        }
 
-    if(i < maxit)
-    {
-        rellenarcerostabla(totalcalculos,i,maxit,7,8,9);
-    }
+        for(i = 0;i < nalpha;i++)
+        {
+            calculosraiz_i[i][0] = i;
+            calculosraiz_i[i][7] = calculosalpha[i][0];
+            calculosraiz_i[i][8] = calculosalpha[i][1];
+            calculosraiz_i[i][9] = fabs(calculosalpha[i+1][0] - calculosalpha[i][0]); //cuidado con el ultimo
+        }
 
-    for(i = 0;i < nb;i++)
-    {
-        totalcalculos[i][0] = i;
-        totalcalculos[i][10] = calculosb[i][0];
-        totalcalculos[i][11] = calculosb[i][1];
-        totalcalculos[i][12] = fabs(calculosb[i+1][0] - calculosb[i][0]); //cuidado con el ultimo
-    }
+        if(i < maxit)
+        {
+            rellenarcerostabla(calculosraiz_i,i,maxit,7,8,9);
+        }
 
-    if(i < maxit)
-    {
-        rellenarcerostabla(totalcalculos,i,maxit,10,11,12);
-    }
+        for(i = 0;i < nb;i++)
+        {
+            calculosraiz_i[i][0] = i;
+            calculosraiz_i[i][10] = calculosb[i][0];
+            calculosraiz_i[i][11] = calculosb[i][1];
+            calculosraiz_i[i][12] = fabs(calculosb[i+1][0] - calculosb[i][0]); //cuidado con el ultimo
+        }
 
-    guardarmegatablanewton(totalcalculos, maxit, 14,"megatablaraiz1.txt");
-    guardarmegatablanewtonlatex(totalcalculos, maxit, 14,"megatablalatexraiz1.txt");
+        if(i < maxit)
+        {
+            rellenarcerostabla(calculosraiz_i,i,maxit,10,11,12);
+        }
+        
+        sprintf(buffer,"%d",j+1);
+        strcat(nombretabla, "megatabla");
+        strcat(nombretabla, buffer);
+        strcat(nombretablalatex, nombretabla);
+        strcat(nombretabla, ".txt");
+        strcat(nombretablalatex, "latex");
+        strcat(nombretablalatex, ".txt");
+
+        guardarmegatablanewton(calculosraiz_i, maxit, 14,nombretabla);
+        guardarmegatablanewtonlatex(calculosraiz_i, maxit, 14,nombretablalatex);
+
+        strcpy(nombretabla, "");
+        strcpy(nombretablalatex, "");
+        strcpy(buffer, "");
+    }
 }
